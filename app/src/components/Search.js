8a4304b73f6d32d73d19a375ca3ead12def667f5
Robert Stevahn <rstevahn@pobox.com>
2020-04-08 14:02:40 -0600

    search path routing fixed
    I needed to import withRouter from the router and wrap my component with that.

--- app/src/components/Search.js
+++ app/src/components/Search.js
@@ -1,10 +1,15 @@
 // jshint esversion: 9
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
-class Search extends Component {    
+class MySearch extends Component {
+    static propTypes = { history: PropTypes.object.isRequired };
+
     handleSubmit = (e) => {
         e.preventDefault();
         const search = this.search.value;
+        this.search.value = null;
         const path = `/${search}`;
         this.props.history.push(path);
     }
@@ -12,7 +17,7 @@ class Search extends Component {
     render() {
         return (
             <form className="search-form" onSubmit={this.handleSubmit}>
-                <input type="search" name="search" placeholder="Search" ref={ (input) => this.search = input } required/>
+                <input type="search" id="search" name="search" placeholder="Search" ref={ (input) => this.search = input } />
                 <button type="submit" className="search-button">
                     <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
@@ -23,5 +28,7 @@ class Search extends Component {
         );
     };
 }
+// eslint-disable-next-line
+const Search = withRouter(MySearch);
 
 export default Search;
